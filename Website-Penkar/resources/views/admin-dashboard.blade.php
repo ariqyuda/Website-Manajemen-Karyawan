37817e66b062d2c18e880b55029704c5ea58298a
Ariq Yuda <ariqyuda22@gmail.com>
2021-11-16 00:11:44 +0700

    First Commit
    Upload beberapa file html, css dan js untuk website RKPL

---
+++ Website-Penkar/resources/views/admin-dashboard.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/background-style.css">
+        <link rel="stylesheet" href="/css/dashboard-admin/dashboard-admin.css">
+        <title>Dashboard - Penkar</title>
+        <script src="/js/menu.js"></script>
+    </head>
+    <body>
+        
+        <nav class="navbar">
+            <div class="left" onclick="openNav()">
+                <img src="/assets/dashboard-admin/menu.png" alt="">
+            </div>
+            <div class="text-left">DATA PEGAWAI</div>
+            <a class="right" href=""><img class="navbar-image" src="/assets/dashboard-admin/back.png" alt=""></a>
+            <a class="right" href=""><img class="navbar-image" src="/assets/dashboard-admin/check.png" alt=""></a>
+            <a class="right" href=""><img class="navbar-image" src="/assets/dashboard-admin/search.png" alt=""></a>
+        </nav>
+        
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">
+                <table>
+                    <tr>
+                        <td><img src="/assets/dashboard-admin/profile.png" alt=""></td>
+                        <td>Akun</td>
+                    </tr>
+                </table>
+            </a>
+            <a href="#information-box">
+                <table>
+                    <tr>
+                        <td><img src="/assets/dashboard-admin/about.png" alt=""></td>
+                        <td>About</td>
+                    </tr>
+                </table>
+            </a>
+        </div>
+
+        <div id="information-box">
+
+                <a href=# class="back">BACK</a>
+        </div>
+        
+    </body>
+</html>
---
+++ Website-Penkar/resources/views/login-page.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/login-page/login-page-style.css">
+        <title>Landing Page - Penkar</title>
+    </head>
+
+    <body>
+        
+        <img class="profile-logo" src="/assets/landing-page/profile-logo.png" alt="profile logo">
+        <p class="profile-text">@PANGMOEDROOFTOP.BNA</p>
+        
+        <div>
+            <img class="p-word-image" src="/assets/landing-page/p-word.png" alt=" ">
+            <p class="penkar-text">enkar</p>
+        </div>
+        <a href="" class="button button-admin">ADMIN</a>
+        <a href="" class="button button-karyawan">KARYAWAN</a>
+        
+    </body>
+</html>
